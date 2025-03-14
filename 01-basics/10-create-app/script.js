33dfd2744499a5d1fb827f2c24d76109fe2ac333
Konstantin Kuznetsov <inmint@mail.ru>
2025-03-14 22:44:45 +0000

    My first app makes current date

--- 01-basics/10-create-app/script.js
+++ 01-basics/10-create-app/script.js
@@ -1,1 +1,41 @@
 import { defineComponent, createApp } from 'vue'
+
+//компонент с выводом локольного времени в локальном формате
+const timeComponent = defineComponent({
+    name: 'timeLocal',
+    setup(){
+
+        const userLocal = navigator.language;
+
+        function formatLocal(){
+            const options = {
+                year: "numeric",
+                month: "long",
+                day: "numeric",
+              };
+            return new Date().toLocaleDateString(userLocal, options)
+        }
+        return {formatLocal, userLocal};
+    },
+
+    template: `Сегодня {{formatLocal()}}`
+    
+});
+
+
+//основной компонент
+// eslint-disable-next-line vue/one-component-per-file
+const App = defineComponent({
+    name: 'myFirstApp',
+
+    components: {
+      timeComponent
+    },
+
+    template: `<timeComponent />`
+  });
+
+
+const app = createApp(App);
+//const vm = app.mount('#app');
+window.vm = app.mount('#app');
